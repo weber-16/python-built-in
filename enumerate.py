5ac63a6313c75a4fa5d3a11442f4828bc6608c48
weber-16 <116704709+weber-16@users.noreply.github.com>
2022-11-10 20:14:03 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -10,3 +10,10 @@ for i, x in enumerate(cars):
 cars = ["Aston" , "Audi", "McLaren "]
 for x in enumerate(cars):
     print (x[0], x[1])
+# also in
+
+# demonstrating the use of start in enumerate
+
+cars = ["Aston" , "Audi", "McLaren "]
+for x in enumerate(cars, start=1):
+    print (x[0], x[1])
