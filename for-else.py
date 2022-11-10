83724dd0f65e12a6ea5d6ef58ad4d5441ffbfb43
weber-16 <116704709+weber-16@users.noreply.github.com>
2022-11-10 19:42:39 +0530

    Create for-else.py

---
+++ for-else.py
@@ -0,0 +1,8 @@
+# Python program to illustrate
+# combining else with for
+
+list = ["geeks", "for", "geeks"]
+for index in range(len(list)):
+    print list[index]
+else:
+    print "Inside Else Block"
