1dba61f0eb4e2992ac864814eafabafa6bbd51d8
weber-16 <116704709+weber-16@users.noreply.github.com>
2022-11-10 20:08:54 +0530

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,6 @@
+'''return (0, cars[0]), (1, cars[1]), (2, cars[2]), and so on.'''
+# Accessing items using enumerate()
+
+cars = ["Aston" , "Audi", "McLaren "]
+for i, x in enumerate(cars):
+    print (x)
