3bb42f7862c92ff52abecc8b2302d7480ddd5728
weber-16 <116704709+weber-16@users.noreply.github.com>
2022-11-10 20:49:58 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -17,3 +17,24 @@ for x in enumerate(cars):
 cars = ["Aston" , "Audi", "McLaren "]
 for x in enumerate(cars, start=1):
     print (x[0], x[1])
+
+    
+# Two separate lists
+cars = ["Aston", "Audi", "McLaren"]
+accessories = ["GPS kit", "Car repair-tool kit"]
+
+# Single dictionary holds prices of cars and 
+# its accessories.
+# First three items store prices of cars and
+# next two items store prices of accessories.
+prices = {1:"570000$", 2:"68000$", 3:"450000$",
+          4:"8900$", 5:"4500$"}
+
+# Printing prices of cars
+for index, c in enumerate(cars, start=1):
+    print "Car: %s Price: %s"%(c, prices[index])
+
+# Printing prices of accessories
+for index, a in enumerate(accessories,start=1):
+    print ("Accessory: %s Price: %s"\
+           %(a,prices[index+len(cars)]))
